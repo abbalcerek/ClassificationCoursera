3f9693300a766b6f4eb50675cffec89717d35fa2
abbalcerek <abbalcerek1@gmail.com>
2016-03-10 00:18:53 +0100

    week1

--- src/week1/ass1/ass1.py
+++ src/week1/ass1/ass1.py
@@ -4,11 +4,11 @@ from src.utils.utils import *
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
-
+#
 pd.set_option('expand_frame_repr', False)
 dtypes = {"name": str, "review": str, "rating": int}
 products = pd.read_csv('data/amazon_baby.csv', dtype=dtypes, sep=',', quotechar='"')
-
+#
 
 def remove_punctuation(text):
     from string import punctuation
@@ -19,16 +19,16 @@ def remove_punctuation(text):
 products['review_clean'] = products['review'].apply(remove_punctuation)
 products = products[products['rating'] != 3]
 products['sentiment'] = products['rating'].apply(lambda rating: 1 if rating > 3 else -1)
-
+#
 train_data_indexes = load_json_list("data/module-2-assignment-train-idx.json")
 train_data = products.iloc[train_data_indexes]
-
+#
 test_data_indexes = load_json_list("data/module-2-assignment-test-idx.json")
 test_data = products.iloc[train_data_indexes]
-
-print(train_data)
-
-
+#
+# print(train_data)
+#
+#
 vectorizer = CountVectorizer(token_pattern=r'\b\w+\b')
 train_matrix = vectorizer.fit_transform(train_data['review_clean'])
 print(train_matrix)
@@ -43,3 +43,21 @@ positive = len([c for c in coefs[0] if c >= 0])
 negative = len([c for c in coefs[0] if c < 0])
 
 print("positive coefs: {}, negative coefs: {}".format(positive, negative))
+#
+sample_test_data = test_data[10:13]
+
+# from sklearn.externals import joblib
+# joblib.dump(sentiment_model, 'data/classifier.pkl')
+
+from sklearn.externals import joblib
+joblib.dump(vectorizer, 'data/vectorizer.pkl')
+
+print(sample_test_data.iloc[0]['review'])
+print(sample_test_data.iloc[1]['review'])
+print(sample_test_data.iloc[2]['review'])
+
+print(sample_test_data)
+
+sample_test_matrix = vectorizer.transform(sample_test_data['review_clean'])
+scores = sentiment_model.decision_function(sample_test_matrix)
+print(scores)
